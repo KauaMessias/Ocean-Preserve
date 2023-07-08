19ed2f17aa6fa5a8bd13798a3e7176d15a59b123
KauaMessias <kauamessias1@gmail.com>
2023-07-08 15:13:13 -0300

    Menu Mobile

--- js/javascript.js
+++
@@ -1,8 +0,0 @@
-const btnMobile = document.getElementById('btn-mobile');
-
-function toggleMenu(){
-    const nav = document.getElementById('nav-mobile');
-    nav.classList.toggle('active');
-}
-
-btnMobile.addEventListener('click', toggleMenu);

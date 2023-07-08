b8676979f1ae3abf3ab81535c864cbfb040233e8
KauaMessias <kauamessias1@gmail.com>
2023-07-08 01:08:43 -0300

    a

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(){
-    const nav = document.getElementById('navigation');
+    const nav = document.getElementById('nav-mobile');
     nav.classList.toggle('active');
 }
 
